aa00a07f72082fa711ab7a3a977f4183e41b8f99
Tiago Santos <t.santos84@gmail.com>
2017-05-07 13:41:24 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -eo pipefail
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+REPOSITORY=tmcsantos/java
+
+die() {
+    echo -e "${RED}$1${NC}" >&2
+    exit 1
+}
+
+info() {
+    echo -e "${GREEN}$1${NC}"
+}
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+    versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+for version in "${versions[@]}"; do
+    TAG=${version}-alpine
+    dockerImage=$REPOSITORY:$TAG
+
+    #docker build --rm --squash -t $dockerImage .
+    #docker rmi $(docker images -a --filter=dangling=true -q) 2>/dev/null
+    echo "building $dockerImage"
+    docker build --rm --pull -t $dockerImage $version 1> /dev/null && \
+    info "done" || die "failed"
+
+    # test
+    echo "testing $dockerImage"
+    docker run --rm $dockerImage java -version 2> /dev/null && \
+    info "good" || die "bad"
+done 
---
+++ update.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -eo pipefail
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+    versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+GLIBC_VERSION="2.25-r0"
+
+alpineVersions[7]='3.5'
+alpineVersions[8]='3.5'
+
+die() {
+    echo $1 >&2
+    exit 1
+}
+
+for version in "${versions[@]}"; do # "8-jdk"
+    javaVersion="${version%%-*}"    # "8"
+    javaType="${version#*-}"       # "jdk"
+
+    alpineVersion="${alpineVersions[$javaVersion]}"
+   
+    oracleBaseURL="http://www.oracle.com"
+    oracleURL="${oracleBaseURL}/technetwork/java/javase/downloads/index.html"
+
+    oracleReleasesURL="https://www.java.com/en/download/faq/release_dates.xml"
+    javaVersionMinor=$(curl -s $oracleReleasesURL | grep "Java $javaVersion Update" | sed "s/.*Update \([0-9]*\).*/\1/g" | head -1)
+
+    case "$javaVersion" in
+        7)
+            # find latest java version from Previous Releases
+            archiveURL=$(curl -s $oracleURL | grep -i "java archive" | grep -i "href" | sed "s|.*href=\"\(.*\)\">\(.*\)|\1|")
+            archiveURL=$oracleBaseURL$archiveURL
+            javaSEURL=$(curl -s $archiveURL | grep "Java SE 7" | sed "s/.*href=\"\(.*\)\">Java SE 7<\/a>.*/\1/")
+            ;;
+        8)
+            javaSEURL=$(curl -s $oracleURL | grep -i "/${javaType}${javaVersion}-downloads" | grep -i "href" | sed "s|.*href=\"\(.*\)\">\(.*\)|\1|" | head -1)
+            ;;
+    esac 
+    javaSEURL=$oracleBaseURL$javaSEURL
+    downloadURL=$(curl -s $javaSEURL | grep "/${javaType}-${javaVersion}u${javaVersionMinor}-linux-x64.tar.gz" | sed "s;.*filepath\":\"\(.*tar\.gz\)\".*;\1;")
+    javaVersionBuild=$(echo $downloadURL | sed "s/.*${javaVersion}u${javaVersionMinor}-b\([0-9]*\).*/\1/" )
+
+    oracleFullVersion=${javaVersion}u${javaVersionMinor}-b${javaVersionBuild}
+    
+    echo "$version: $oracleFullVersion (alpine $alpineVersion)"
+    
+    dockerfileTemplate="Dockerfile.template"
+    [ ! -f ${dockerfileTemplate} ] && die "Missing Dockerfile template: $dockerfileTemplate"
+
+    sed \
+        -e "s/%ALPINE_VERSION%/${alpineVersion}/g" \
+        -e "s/%JVM_MAJOR%/${javaVersion}/g" \
+        -e "s/%JVM_MINOR%/${javaVersionMinor}/g" \
+        -e "s/%JVM_BUILD%/${javaVersionBuild}/g" \
+        -e "s/%JVM_TYPE%/${javaType}/g" \
+        -e "s|%JVM_DOWNLOAD_URL%|${downloadURL}|g" \
+        -e "s/%GLIBC_VERSION%/${GLIBC_VERSION}/g" \
+        ${dockerfileTemplate} > $version/Dockerfile
+done
